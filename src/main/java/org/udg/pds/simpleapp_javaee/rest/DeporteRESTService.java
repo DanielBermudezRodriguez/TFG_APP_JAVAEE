6526b4b0c5fa073c5014ea530602e7fcb87af7f5
DaniBermudezRodriguez <dani_bermu12@hotmail.com>
2018-04-07 10:46:16 +0200

    Eliminada obligación de estar logeado para obtener los deportes disponibles

--- src/main/java/org/udg/pds/simpleapp_javaee/rest/DeporteRESTService.java
+++ src/main/java/org/udg/pds/simpleapp_javaee/rest/DeporteRESTService.java
@@ -7,7 +7,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -27,12 +26,7 @@ public class DeporteRESTService extends GenericRESTService {
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response obtenerDeportes(@Context HttpServletRequest req) {
-		if (estaUsuarioLogeado(req)) {
-			return buildResponse(deporteService.obtenerDeportes());
-		} else {
-			throw new WebApplicationException("No ha iniciado sesión.");
-		}
-
+		return buildResponse(deporteService.obtenerDeportes());
 	}
 
 }
