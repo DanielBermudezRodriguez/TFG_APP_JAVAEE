7a91f3f684948db558a9f93ca1550ea9b6c8dd61
DaniBermudezRodriguez <dani_bermu12@hotmail.com>
2018-08-28 18:52:52 +0200

    JavaDoc hashpassword

--- src/main/java/org/udg/tfg/javaee/util/HashPassword.java
+++ src/main/java/org/udg/tfg/javaee/util/HashPassword.java
@@ -4,12 +4,30 @@ import java.math.BigInteger;
 import javax.crypto.SecretKeyFactory;
 import javax.crypto.spec.PBEKeySpec;
 
+/**
+ * Esta clase se encarga de cifrar y validar las contraseñas de los usuarios de la aplicación.
+ * @author: Daniel Bermudez Rodriguez
+ * @version: 1.0
+ */
 public final class HashPassword {
 
+	/**
+	 * Número de iteraciones a realizar a la hora de cifrar una contraseña.
+	 */
 	private static final int NUMERO_ITERACIONES = 1000;
 
+	/**
+	 * Tamaño en Bytes de la contraseña cifrada.
+	 */
 	private static final int LONGITUD_HASH = 512;
 
+	/**
+	* Método encargado de cifrar una contraseña.
+	* @param password valor de la contraseña a cifrar.
+	* @param salt valor de la salt a utilizar en el proceso de cifrado.
+	* @return Devuelve la contraseña cifrada o null si se ha producido algun error durante el proceso de cifrado.
+	* @exception e en caso de que se produzca algun error en el proceso de cifrado.
+	*/
 	public static String passwordHash(String password, String salt) {
 		try {
 			char[] chars = password.toCharArray();
@@ -22,6 +40,13 @@ public final class HashPassword {
 		}
 	}
 
+	/**
+	* Método encargado de validar una contraseña de un usuario en el momento de iniciar sesión.
+	* @param originalPassword valor de la contraseña introducida al iniciar sesión.
+	* @param salt valor de la salt a utilizar en el proceso de cifrado.
+	* @param storedPassword valor de la contraseña cifrada en base de datos.
+	* @return Devuelve true si al cifrar la contraseña originalPassword coincide con el valor de storedPassword.
+	*/
 	public static boolean validarPassword(String originalPassword, String salt, String storedPassword) {
 		return passwordHash(originalPassword, salt).equals(storedPassword);
 	}
